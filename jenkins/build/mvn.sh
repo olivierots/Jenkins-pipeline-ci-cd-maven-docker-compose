807b770d4e1d8bf48da0e2c1eee2e06627662827
jenkins <jenkins@Olivier.home>
2019-09-30 00:17:39 +0100

    fixes workspace path for job executions

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,4 +3,6 @@
 echo "********************************************************"
 echo "*********************** Building jar *******************"
 
-docker run --rm -v  $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
